538989f7f37680c996f1d5e71c8c086eabe52f71
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-09-21 16:40:03 +0000

    Done.

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,7 +1,9 @@
-describe "Hello World" do 
+describe "Hello World" do
+
 
   it 'puts "Hello World!"' do
-    # $stdout represents the output of your program, don't worry about how
+    puts "Hello World!"
+      # $stdout represents the output of your program, don't worry about how
     # we are testing this, focus on simply coding your solution.
     expect($stdout).to receive(:puts).with("Hello World!")
     load './hello_world.rb'
