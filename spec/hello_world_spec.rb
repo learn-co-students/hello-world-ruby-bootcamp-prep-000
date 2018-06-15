ac082e885a10e4f3af9914c807c6eebbeb5d239e
bilojan91@gmial.com <bilojan91@gmial.com>
2018-06-15 07:36:33 +0000

    Done.

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,3 +1,4 @@
+puts"hello_world"
 describe "Hello World" do 
   it 'has a file hello_world.rb' do
     expect(File.exists?("hello_world.rb")).to be_truthy, "Did you create the file hello_world.rb? (If so, is it spelled exactly the same with the same capitalization?)"
