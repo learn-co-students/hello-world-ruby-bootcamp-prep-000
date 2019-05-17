3f4c54100c6e5b5885cd86ed25c605127afe4e13
github username <github email address>
2019-05-17 15:36:53 -0400

    Done.

--- hello_world.rb
+++ hello_world.rb
@@ -1,13 +1,2 @@
 
 puts "Hello World!"
-
-
-=begin 
-.rb is the extension used 
-for ruby files. Comments are also
-tagged differentlly than in javascript
-and html. But the =begin and =end tags
-must be on their own lines!
-=end
-
-
